ed7f695f8e6f1af8e86d269bef92de2758a7325d
Lisa Zhao <ruohongzhao.lisa@gmail.com>
2026-03-21 18:02:58 +0100

    Set constants

---
+++ src/config.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+DATA_PATH = Path(__file__).parent.parent / "data" / "StudentPerformanceFactors.csv"
+
+TARGET = "Exam_Score"
+RANDOM_STATE = 42
+TEST_SIZE = 0.2
+CV_FOLDS = 10
+
+MISSING_COLS = [
+    "Parental_Education_Level",
+    "Teacher_Quality",
+    "Distance_from_Home",
+]
+
+DEPTHS = list(range(1, 21))
